4d954debf43c40a17040365762dd27bf80dd7754
ArunKumar Kandasamy <k.arunkumar@outlook.in>
2025-05-26 00:30:38 +0530

    ENHANCEMENT| jwt expiration time removed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -320,6 +320,6 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       [ "DELETE", %r{^/users/sign_out$} ]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    # jwt.expiration_time = 30.minutes.to_i
   end
 end
